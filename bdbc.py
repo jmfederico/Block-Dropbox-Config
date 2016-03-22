c203bc0ec260f7962f17c1e84afa63e53c885f37
Federico Jaramillo Martínez <federicojaramillom@gmail.com>
2016-03-21 21:37:54 -0500

    Single instance and LICENSE

--- bdbc.py
+++ bdbc.py
@@ -1,4 +1,7 @@
 from pywinauto import Application, findwindows
+from tendo import singleton
+
+me = singleton.SingleInstance() # will sys.exit(-1) if other instance is running
 
 while True:
     try:
